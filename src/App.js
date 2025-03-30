e442a03d683c2b26a54e7cdcc0557338519f6d0b
sameerdiwse <sameerdiwse9090@gmail.com>
2025-03-30 16:52:02 +0530

    Styling Completed

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ function App() {
       <div className="container">
         <div className="todo">
           <p>Build React App!</p>
+          <div className='actions'>
+            <input type='checkbox'></input>
+            <button>Delete</button>
+          </div>
         </div>
       </div>
     </main>
