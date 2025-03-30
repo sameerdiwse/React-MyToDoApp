ab173c193bcf575218691076c54e1ea923dffff6
sameerdiwse <sameerdiwse9090@gmail.com>
2025-03-30 10:26:04 +0530

    Initial Push of my to do list app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>My To Do App</h1>
+
+      <div className="input-container">
+        <input type="text"></input>
+        <button>+</button>
+      </div>
+    </main>
+   );
+}
+
+export default App;
